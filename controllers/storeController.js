77bedab2981a742223745b57452847e4fc663314
fatemaAlbasri <falbasri4@gmail.com>
2025-08-05 10:21:12 +0300

    Added storeControllers and store route

---
+++ controllers/storeController.js
@@ -0,0 +1,16 @@
+const stores = require('../data/stores.json')
+const Review = require('../models/review')
+
+exports.store_getAll = async (req, res) => {
+  res.json(stores)
+}
+
+exports.store_getById = async (req, res) => {
+  const storeId = parseInt(req.params.id)
+
+  const store = stores.find((store) => store.storeId === storeId)
+  if (!store) res.status(401).send({ status: 'Error', msg: 'no store found' })
+
+  const review = await Review.find({ storeId: id })
+  res.json({ ...store, review })
+}
